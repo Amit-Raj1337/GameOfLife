1ee4624e4e4b908dca09fd5033b2a14f23f9dae2
Amit-Raj1337 <amit.rj1337@gmail.com>
2017-02-10 18:14:26 +0530

    Update Main.java
    Updated the main program corresponding to the new GUI incorporated for GoL

--- GameOfLife/src/Main.java
+++ GameOfLife/src/Main.java
@@ -1,67 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
-public class Main {
+public class main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
 		
 		GoL obj=new GoL();
-		int flag=1;
-		char s;
-		String input;
-		int countOfLivingCells;
 		
-	System.out.println("Welcome to the game of life");
-	System.out.println("Please enter the blocks you wish to insert a living 'cell' into  (By default all cells are dead)");
-	
-	obj.initializeValues();
-	obj.printGrid();
-	
-	System.out.println("Please enter the number of living cells to be inserted");
-	InputStreamReader rd =new InputStreamReader(System.in);
-	BufferedReader br=new BufferedReader(rd);
-	input=br.readLine();
-	countOfLivingCells=Integer.parseInt(input); //converting the string value to integer.
-	
-	obj.insertLivingCells(countOfLivingCells); //calling the function which will insert the living cells.
-	
-	
-	
-	System.out.println();
-	
-	
-	System.out.println("This is the First Instance of the grid after insertion before processing:");
-	
-	obj.printGrid();
-	
-	System.out.println();
-	while(flag!=0)
-	{
-	System.out.println("This is the next instance:");
-	
-	obj.nextInstanceOfGoL();
-	
-	obj.printGrid();
-	
-	System.out.println("Would you like to continue? Enter 'Y' or 'N' ");
-	input=br.readLine();
-	s=input.charAt(0);
-	
-	if((s=='y'||s=='Y'))
-	{
-		obj.copyGrid();
-	
+		obj.initializeValues();
+		obj.LayoutGenerator();
+		obj.gridbuttonLogic();
+		obj.StartButtonLogic();
+		obj.nextButtonLogic();
+		obj.resetButtonLogic();
 		
+	//	EyeHell obj=new EyeHell();
+	//	obj.test2();
 		
-		}
-	else{
-		flag=0;
-	}
-	
-	}
-		
-	
 	}
 
 }
